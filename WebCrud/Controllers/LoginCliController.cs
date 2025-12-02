dfcf0bb2f07691c48d97bc95378c048ca1ceb0e1
Joao Pedro <whitenoob5@gmail.com>
2025-12-02 16:52:00 -0300

    fazendo a página para login do cliente e adicionando ela no home

---
+++ WebCrud/Controllers/LoginCliController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebCrud.Models;
+using MySql.Data.MySqlClient;
+
+namespace WebCrud.Controllers
+{
+    public class LoginCliController : Controller
+    {
+
+        private readonly IConfiguration _configuration;
+        public LoginCliController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // exibe o formulário de cadastro
+        [HttpGet]
+        public IActionResult CadastrarCli() // renomeando de Cadastrar para CadastrarCli
+        {
+            // O sistema procura por Views/LoginCli/CadastrarCli.cshtml
+            return View();
+        }
+
+        // processa o cadastro do Cliente
+        [HttpPost]
+        public IActionResult CadastrarCli(Cliente cliente) // renomeando Cadastrar para CadastrarCli
+        {
+    
+            string? connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            // usando essa conexão para garantir uma conexão com o SQL
+            using var connection = new MySqlConnection(connectionString);
+            {
+                connection.Open();
+
+                // comando SQL pras respectivas colunas da nossa tb cliente
+                string sql = "INSERT INTO cliente (NomeCli, EmailCli, EnderecoCli, TelefoneCli) " +
+                             "VALUES (@NomeCli, @EmailCli, @EnderecoCli, @TelefoneCli)";
+
+                using MySqlCommand command = new MySqlCommand(sql, connection);
+                {
+                    // parâmetros para o Cliente ser cadastrado
+                    command.Parameters.AddWithValue("@NomeCli", cliente.NomeCli);
+                    command.Parameters.AddWithValue("@EmailCli", cliente.EmailCli);
+                    command.Parameters.AddWithValue("@EnderecoCli", cliente.EnderecoCli);
+                    command.Parameters.AddWithValue("@TelefoneCli", cliente.TelefoneCli);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            // redireciona após o cadastro do cliente
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ WebCrud/Models/LoginCli.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebCrud.Models
+{
+    public class Cliente
+    {
+        [Display(Name = "Nome do Cliente")]
+        public string? NomeCli { get; set; }
+
+        [Display(Name = "Email do Cliente")]
+        public string? EmailCli { get; set; }
+
+        [Display(Name = "Endereco do Cliente")]
+        public string? EnderecoCli { get; set; }
+
+        [Display(Name = "Telefone do Cliente")]
+        public string? TelefoneCli { get; set; }
+    }
+}
