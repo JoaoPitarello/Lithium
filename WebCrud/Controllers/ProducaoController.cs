d03fd486c1d1661dafad6646379ae53d6e647d44
Joao Pedro <whitenoob5@gmail.com>
2025-12-04 11:00:21 -0300

    adicionando botão para produção na home e a model, view e controller da Produção juntamente com sua função de cadastrar

---
+++ WebCrud/Controllers/ProducaoController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebCrud.Models;
+using MySql.Data.MySqlClient;
+
+namespace WebCrud.Controllers
+{
+    public class ProducaoController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public ProducaoController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult CadastrarProdu()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CadastrarProdu(Producao producao)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(producao);
+            }
+
+            string? connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            using var connection = new MySqlConnection(connectionString);
+            {
+                try
+                {
+                    connection.Open();
+
+                    string sql = "INSERT INTO tbProducao (data_inicio, data_fim, qtd_material, fkfuncionarioid) " +
+                                 "VALUES (@IniProdu, @FimProdu, @QtdProdu, @Fkfuncionarioid)";
+
+                    using MySqlCommand command = new MySqlCommand(sql, connection);
+                    {
+                        command.Parameters.AddWithValue("@IniProdu", producao.IniProdu);
+                        command.Parameters.AddWithValue("@FimProdu", producao.FimProdu);
+                        command.Parameters.AddWithValue("@QtdProdu", producao.QtdProdu);
+                        command.Parameters.AddWithValue("@Fkfuncionarioid", producao.Fkfuncionarioid);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+                catch (MySqlException ex)
+                {
+                    ModelState.AddModelError(string.Empty, "Erro ao registrar a produção no banco de dados: " + ex.Message);
+                    return View(producao);
+                }
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ WebCrud/Models/Producao.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebCrud.Models
+{
+    public class Producao
+    {
+        [Required(ErrorMessage = "É obrigatório o início da produção.")]
+        [Display(Name = "Início da produção")]
+        public DateTime IniProdu { get; set; }
+
+        [Required(ErrorMessage = "É obrigatório o fim da produção.")]
+        [Display(Name = "Fim da produção")]
+        public DateTime FimProdu { get; set; }
+
+        [Required(ErrorMessage = "É obrigatória a quantidade de material.")]
+        [Display(Name = "Quantidade de materiais usados")]
+        public string? QtdProdu { get; set; }
+
+        [Required(ErrorMessage = "O funcionario é obrigatório.")]
+        [Display(Name = "ID do Funcionario")]
+        public int Fkfuncionarioid { get; set; }
+    }
+}
