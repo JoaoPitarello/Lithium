e7d265bee2945067850b3a650b61d9cf8e2bfacb
Joao Pedro <whitenoob5@gmail.com>
2025-12-03 23:04:24 -0300

    fazendo mais modificações na home e adicionando a função de cadastrar os fornecedores

---
+++ WebCrud/Controllers/FornecedorController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebCrud.Models;
+using MySql.Data.MySqlClient;
+
+namespace WebCrud.Controllers
+{
+    public class FornecedorController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public FornecedorController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult CadastrarForn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CadastrarForn(Fornecedor fornecedor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(fornecedor);
+            }
+
+            string? connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            using var connection = new MySqlConnection(connectionString);
+            {
+                try
+                {
+                    connection.Open();
+
+                    string sql = "INSERT INTO tbFornecedor (nome, endereco, telefone, email) " +
+                                 "VALUES (@Nome, @Endereco, @Telefone, @Email)";
+
+                    using MySqlCommand command = new MySqlCommand(sql, connection);
+                    {
+                        command.Parameters.AddWithValue("@Nome", fornecedor.NomeFor);
+                        command.Parameters.AddWithValue("@Endereco", fornecedor.EnderecoFor);
+                        command.Parameters.AddWithValue("@Telefone", fornecedor.TelefoneFor);
+                        command.Parameters.AddWithValue("@Email", fornecedor.EmailFor);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+                catch (MySqlException ex)
+                {
+                    ModelState.AddModelError(string.Empty, "Erro ao registrar o fornecedor no banco de dados: " + ex.Message);
+                    return View(fornecedor);
+                }
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ WebCrud/Models/Fornecedor.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebCrud.Models
+{
+    public class Fornecedor
+    {
+        [Required(ErrorMessage = "O nome do fornecedor é obrigatório.")]
+        [Display(Name = "Nome do fornecedor")]
+        public string? NomeFor { get; set; }
+
+        [Required(ErrorMessage = "O endereco do fornecedor é obrigatório.")]
+        [Display(Name = "Endereco do fornecedor")]
+        public string? EnderecoFor { get; set; }
+
+        [Required(ErrorMessage = "O Telefone é obrigatório.")]
+        [Display(Name = "Telefone do fornecedor")]
+        public int TelefoneFor { get; set; }
+
+        [Required(ErrorMessage = "O email é obrigatório.")]
+        [EmailAddress(ErrorMessage = "O formato do Email é inválido.")]
+        [Display(Name = "E-mail do fornecedor")]
+        public string? EmailFor { get; set; }
+    }
+}
