838b73022882b1feebd6c994965d48740bca48e0
Joao Pedro <whitenoob5@gmail.com>
2025-12-04 09:59:59 -0300

    adicionando o botão para cadastrar material no layout da home, alterando forma de cadastrar automaticamente o salário do funcionário (mudando o parâmetro step para 100) e fazendo a funcionalidade de adicionar os materiais à base de dados

---
+++ WebCrud/Controllers/MaterialController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebCrud.Models;
+using MySql.Data.MySqlClient;
+
+namespace WebCrud.Controllers
+{ 
+
+    public class MaterialController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public MaterialController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult CadastrarMat()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CadastrarMat(Material material)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(material);
+            }
+
+            string? connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            using var connection = new MySqlConnection(connectionString);
+            {
+                try
+                {
+                    connection.Open();
+                    string sql = "INSERT INTO tbMaterial (nome_material, qtd_estoque, validade, fkFornecedorid) " +
+                                 "VALUES (@NomeMat, @QuantMat, @ValiMat, @Fkfornecedorid)";
+
+                    using MySqlCommand command = new MySqlCommand(sql, connection);
+                    {
+                        command.Parameters.AddWithValue("@NomeMat", material.NomeMat);
+                        command.Parameters.AddWithValue("@QuantMat", material.QuantMat);
+                        command.Parameters.AddWithValue("@ValiMat", material.ValiMat);
+                        command.Parameters.AddWithValue("@Fkfornecedorid", material.Fkfornecedorid);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+                catch (MySqlException ex)
+                {
+                    ModelState.AddModelError(string.Empty, "Erro ao registrar o material no banco de dados: " + ex.Message);
+                    return View(material);
+                }
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ WebCrud/Models/Material.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebCrud.Models
+{
+    public class Material
+    {
+        [Required(ErrorMessage = "O nome do material é obrigatório.")]
+        [Display(Name = "Nome do material")]
+        public string? NomeMat { get; set; }
+
+        [Required(ErrorMessage = "A quantidade em estoque é obrigatória.")]
+        [Display(Name = "Quantidade em estoque")]
+        public int QuantMat { get; set; }
+
+        [Required(ErrorMessage = "A validade do produto é obrigatória.")]
+        [Display(Name = "Validade do material")]
+        public DateTime ValiMat { get; set; }
+
+        [Required(ErrorMessage = "O fornecedor é obrigatório.")]
+        [Display(Name = "ID do Fornecedor")]
+        public int Fkfornecedorid { get; set; }
+    }
+}
