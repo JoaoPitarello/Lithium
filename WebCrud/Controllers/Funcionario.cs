ea0b4b0b3d2298ceb2ed820723b77c4be909b492
Joao Pedro <whitenoob5@gmail.com>
2025-12-03 22:25:56 -0300

    adicionando botão de adicionar funcionário e fazendo o sistema para o cadastro de funcionários na base de dados

---
+++ WebCrud/Controllers/Funcionario.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebCrud.Models;
+using MySql.Data.MySqlClient;
+
+namespace WebCrud.Controllers
+{
+    public class FuncionarioController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public FuncionarioController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult CadastrarFun()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CadastrarFun(Funcionario funcionario)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(funcionario);
+            }
+
+            string? connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            using var connection = new MySqlConnection(connectionString);
+            {
+                try
+                {
+                    connection.Open();
+                    string sql = "INSERT INTO tbFuncionario (salario, nome, endereco, telefone, email) " +
+                                 "VALUES (@Salariofun, @Nomefun, @Enderecofun, @Telefonefun, @Emailfun)";
+
+                    using MySqlCommand command = new MySqlCommand(sql, connection);
+                    {
+                        command.Parameters.AddWithValue("@Salariofun", funcionario.Salariofun);
+                        command.Parameters.AddWithValue("@Nomefun", funcionario.Nomefun);
+                        command.Parameters.AddWithValue("@Enderecofun", funcionario.Enderecofun);
+                        command.Parameters.AddWithValue("@Telefonefun", funcionario.Telefonefun);
+                        command.Parameters.AddWithValue("@Emailfun", funcionario.Emailfun);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+                catch (MySqlException ex)
+                {
+                    ModelState.AddModelError(string.Empty, "Erro ao registrar o funcionário no banco de dados: " + ex.Message);
+                    return View(funcionario);
+                }
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ WebCrud/Models/Funcionario.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebCrud.Models
+{
+    public class Funcionario
+    {
+        [Required(ErrorMessage = "O salário do funcíonário é obrigatório.")]
+        [Display(Name = "Salário do funcionário")]
+        public decimal Salariofun { get; set; }
+
+        [Required(ErrorMessage = "O nome do funcionário é obrigatório.")]
+        [Display(Name = "Nome do funcionário")]
+        public string? Nomefun { get; set; }
+
+        [Required(ErrorMessage = "O Endereço é obrigatório.")]
+        [Display(Name = "Endereco do funcionário")]
+        public string? Enderecofun { get; set; }
+
+        [Required(ErrorMessage = "O Telefone é obrigatório.")]
+        [Display(Name = "Telefone do funcionário")]
+        public string? Telefonefun { get; set; }
+
+        [Required(ErrorMessage = "O email é obrigatório.")]
+        [EmailAddress(ErrorMessage = "O formato do Email é inválido.")]
+        [Display(Name = "E-mail do funcionário")]
+        public string? Emailfun { get; set; }
+    }
+}
